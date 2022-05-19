ce53b1f826fd7ae9dd926faff17bd2eb1e8e56ed
Joe Mama <102310270+schooldev49@users.noreply.github.com>
2022-05-19 02:33:34 +0000

    chadded on

--- index.mjs
+++ index.mjs
@@ -4,27 +4,17 @@ import nodeStatic from 'node-static';
 
 const bare =  new Server('/bare/', '');
 const serve = new nodeStatic.Server('public/');
-const lightspeedserve = new nodeStatic.Server('mathhelp/');
+
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-  var isLS = ip.startsWith('34.216.110') || ip.startsWith('54.244.51') || ip.startsWith('54.172.60') || ip.startsWith('34.203.250') || ip.startsWith('34.203.254');
-
-     if (isLS){
-    lightspeedserve.serve(request, response);
-     }
-  else {
-    if (bare.route_request(request, response))
-      return true;
-  
+    if (bare.route_request(request, response)) return true;
     serve.serve(request, response);
-  }
 });
 
 server.on('upgrade', (req, socket, head) => {
 	if(bare.route_upgrade(req, socket, head))return;
-	socket.end();
+	socket.end(); 
 });
 
-server.listen(443);
+server.listen(process.env.PORT || 443);
---
+++ public/main.asm
@@ -0,0 +1,28 @@
+mov 3, db
+msg db 5
+aux 5, 3
+// memory allocation for AUX
+// 
+/*
+@Params 
+Badcode.exe
+*&/
+msg db "E"
+string e
+  connect "E"
+
+public unsignd integer UID;
+aux 3, 7;
+// boot from DISK since os requires too much time
+
+get * from using db * Disk /linux/theb/
+
+if (not &*) return This.objectTypes.nullifier
+
+This.Prototypes["OS"] = {
+  ["Out", "In"][1] = [] //  
+  ["Boot"] = [
+    This.Prototypes["Native"].GetAttribute("GarbageCollection"):Initiate(); 
+  ],
+
+}
